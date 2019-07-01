5053e6479bef1c93c119240d665e013f87cc72ed
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-07-01 09:26:13 -0600

    working on nav layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'tachyons';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className="min-vh-100 bg-green"
+    >
+    <header class="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+    <nav class="f6 fw6 ttu tracked">
+      <a class="link dim white dib mr3" href="#" title="Home">Home</a>
+      <a class="link dim white dib mr3" href="#" title="About">About</a>
+      <a class="link dim white dib mr3" href="#" title="Store">Store</a>
+      <a class="link dim white dib" href="#" title="Contact">Contact</a>
+    </nav>
+  </header>
     </div>
   );
 }
